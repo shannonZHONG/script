1aa635871c6b53bedcb39ce8dd9ef2140b392e6c
shannon <zhongshiran@gmail.com>
2017-12-26 17:18:45 +0800

    add var path

--- node.js
+++ node.js
@@ -2,18 +2,21 @@ var fs = require('fs')
 
 var dirName = process.argv[2]
 
-if (fs.existsSync("./" + dirName)) {
-    process.exit(1)
-    
-} else {
-process.chdir("/Users/zhongshiran/Desktop")
-fs.mkdirSync("./" + dirName)
-process.chdir("./" + dirName)
-fs.mkdirSync('css')
-fs.mkdirSync('js')
+process.chdir("/Users/zhongshiran/Desktop") 
 
-fs.writeFileSync("index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
-fs.writeFileSync("css/style.css", "h1{color: red;}")
-fs.writeFileSync("js/main.js", "var string = 'Hello World'\nalert(string)")
-process.exit(0)
+var path = "./" + dirName
+
+if (fs.existsSync(path)) {
+    console.log('File already exist！')
+    process.exit(1)
+} else {
+    fs.mkdirSync("./" + dirName)
+    process.chdir("./" + dirName) 
+    fs.mkdirSync('css') 
+    fs.mkdirSync('js') 
+
+    fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+    fs.writeFileSync("css/style.css", "h1{color: red;}")
+    fs.writeFileSync("./js/main.js", "var string=\"Hello World\"\nalert(string)")
+    process.exit(0)
 }
